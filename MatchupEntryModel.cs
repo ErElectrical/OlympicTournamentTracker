632782f181fe7b7f40568012c601364412fb67fe
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-04 12:28:59 +0000

    Added : MatchupEntryModel

---
+++ MatchupEntryModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in Matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents score for this Paricular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents a Matchup from that team comes as winner
+        /// 
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
