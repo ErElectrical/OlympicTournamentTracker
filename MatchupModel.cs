a2c7e20e69dec8e6281989230792805fd7ea8f5c
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-04 12:27:33 +0000

    Added : MatchupModel

---
+++ MatchupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents the Team which comes to play Tournament
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; }
+
+        /// <summary>
+        /// Represents the Winner
+        /// </summary>
+        public TeamModel winner { get; set; }
+
+        /// <summary>
+        /// Represents the number of MatchupRound
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
