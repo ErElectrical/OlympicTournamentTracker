e9de4415f762dba65e3281866d1c90c814f1361c
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-04 07:15:49 +0000

    Added : Main class For TournamentTracker

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTracker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Olympics Game ");
+        }
+    }
+}
