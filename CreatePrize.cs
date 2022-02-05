7f63281b541f3dbe7d8273c7cb6feb43aa1b6ea1
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-05 06:39:52 +0000

    Added : Create Prize Form

---
+++ CreatePrize.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TournamentTrackerUI
+{
+    public partial class CreatePrize : Form
+    {
+        public CreatePrize()
+        {
+            InitializeComponent();
+        }
+
+        private void HeaderLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlaceNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrizePercentageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrizePercentageValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
